6315480b3b96c5ecf3804ff6ced17090b83f27fb
Terencio Agozzino <terencio.agozzino@gmail.com>
2020-07-22 21:42:09 +0200

    Prettier for ComponentForm

--- src/components/form/ComponentForm.tsx
+++ src/components/form/ComponentForm.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react';
-import {ComponentCategory} from "../../constants/componentCategory";
-import ProcessorForm from "./ProcessorForm";
+import { ComponentCategory } from '../../constants/componentCategory';
+import ProcessorForm from './ProcessorForm';
+import TargetForm from './TargetForm';
 
 export interface FormProps {
-  onUpdate: (data: any) => void,
-  onClose: () => void,
+  onClose: () => void;
+  onUpdate: (data: any) => void;
   component: any;
 }
 
-const ComponentForm = (props : FormProps) => {
+const ComponentForm = (props: FormProps) => {
   const { category } = props.component;
   if (category === ComponentCategory.Target) {
-    return null;
+    return <TargetForm {...props} />;
   } else if (category === ComponentCategory.Processor) {
     return <ProcessorForm {...props} />;
   } else if (category === ComponentCategory.Source) {
     return null;
   }
-
   return null;
 };
 
